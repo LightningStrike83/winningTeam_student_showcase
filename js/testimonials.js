368fac5c5435b5618b3c4b7a93b4487c9e1a7c7f
Dan Singer <d_singer@fanshaweonline.ca>
2023-09-07 00:28:04 -0400

    Arrays and Edits
    Added the arrays and templates, also changed a few things based on feedback

---
+++ js/testimonials.js
@@ -0,0 +1,22 @@
+let testimonials = [
+    {
+        testimonial1 : "",
+        testimonial2 : "",
+        testimonial3 : "",
+        testimonial4 : "",
+    },
+
+    {
+        testimonial1 : "",
+        testimonial2 : "",
+        testimonial3 : "",
+        testimonial4 : "",
+    },
+
+    {
+        testimonial1 : "",
+        testimonial2 : "",
+        testimonial3 : "",
+        testimonial4 : "",
+    },
+]
