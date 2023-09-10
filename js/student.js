50a3fbcb0835bdfa9e353a718fb20f979f9daabc
Dan Singer <d_singer@fanshaweonline.ca>
2023-09-10 12:31:17 -0400

    More Updates
    Updates to everything, nearing to final completion

--- js/student.js
+++ js/student.js
@@ -3,6 +3,7 @@ let portfolioButton = document.querySelector ('#portfolio-button');
 let idp2024 = document.querySelector('#year-2024');
 let idp2023 = document.querySelector('#year-2023');
 let idp2022 = document.querySelector('#year-2022');
+let lightboxStudent = document.querySelectorAll('.project-lightbox');
 
 function displayWork () {
   let examplesOne = document.querySelector ('#stu-examples-row-1')
@@ -24,6 +25,7 @@ function displayWork () {
 function displayPortfolio() {
     let studentList = document.querySelectorAll('#stu-por-list-con a');
     let yearSelector = document.querySelector('#year-selector');
+    let currentYear = document.querySelector('#year-display');
 
     studentList.forEach(item => {
         if (item.style.display === 'none' || item.style.display === '') {
@@ -38,11 +40,18 @@ function displayPortfolio() {
       } else {
         yearSelector.style.display = 'flex';
       }
+
+      if (currentYear.style.display === 'block') {
+        currentYear.style.display = 'none';
+      } else {
+        currentYear.style.display = 'block';
+      }
 }
 
 function displayStudents () {
     let studentName = this.dataset.member
 
+    document.querySelector('#current-year').textContent= idp_students [studentName].year
     document.querySelector('#name-1').textContent= idp_students [studentName].namea
     document.querySelector('#name-2').textContent= idp_students [studentName].nameb
     document.querySelector('#name-3').textContent= idp_students [studentName].namec
@@ -61,8 +70,23 @@ function displayStudents () {
     document.querySelector('#name-16').textContent= idp_students [studentName].namep
 }
 
+function openStudentLightbox() {
+    let studentLightbox = this.dataset.member
+
+    document.querySelector('#lightbox-student').style.display = 'grid';
+
+    document.querySelector('#lb-title-student').textContent= project_members [studentLightbox].projectname
+    document.querySelector('#lb-description').textContent= project_members [studentLightbox].description
+    document.querySelector('#lb-team').textContent= project_members [studentLightbox].members
+    document.querySelector('#lb-link1').textContent= project_members [studentLightbox].link1
+    document.querySelector('#lb-link2').textContent= project_members [studentLightbox].link2
+    document.querySelector('#lb-link3').textContent= project_members [studentLightbox].link3
+    document.querySelector('#lb-link4').textContent= project_members [studentLightbox].link4
+}
+
 workButton.addEventListener('click', displayWork)
 portfolioButton.addEventListener('click', displayPortfolio)
 idp2024.addEventListener('click', displayStudents)
 idp2023.addEventListener('click', displayStudents)
 idp2022.addEventListener('click', displayStudents)
+lightboxStudent.forEach(lightbox => lightbox.addEventListener('click', openStudentLightbox))
